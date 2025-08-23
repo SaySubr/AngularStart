4926c4d5b7c69c689bf31b253397cfc68689cfe9
SaySubr <165163931+SaySubr@users.noreply.github.com>
2025-08-23 19:55:12 +0700

    add routing between Achievements and About
    Home Work for 22.08.2025

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { AchievementsComponent } from './components/achievements/achievements.component';
-import { WeatherModalComponent } from './components/weather-modal/weather-modal.component';
-import { AboutComponent } from './components/about/about.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routes';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [ HeaderComponent,
-    FooterComponent,
-    AchievementsComponent,
-    WeatherModalComponent,
-    AboutComponent],
+  HeaderComponent, FooterComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AchievementsComponent } from './components/achievements/achievements.component';
+import { AboutComponent } from './components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'achievements', component: AchievementsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: 'achievements', pathMatch: 'full' }, 
+  { path: '**', redirectTo: 'achievements' } 
+];
