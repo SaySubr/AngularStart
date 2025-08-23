56687eb57db9f0f4da8bc08260dc1776f4a14f2a
SaySubr <165163931+SaySubr@users.noreply.github.com>
2025-08-23 18:56:10 +0700

    Create Angular five Components
    Home Work for 11.08.2025

---
+++ src/app/components/weather-modal/weather-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './weather-modal.component.html',
+  styleUrl: './weather-modal.component.scss'
+})
+export class WeatherModalComponent {
+
+}
